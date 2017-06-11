e0b92113aeadd65b0727689f597e84a30e797ee0
Alex Raison <alexraison@gmail.com>
2017-06-11 15:24:12 +0100

    Add customer list, user list and outline user detail page.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-from app.views import mod as baseModule
-app.register_blueprint(baseModule)
 
-from app.userManagement.views import mod as userManagementModule
-app.register_blueprint(userManagementModule)
+from app import views
--- app/views.py
+++ app/views.py
@@ -2,51 +2,33 @@ from flask import Blueprint, session, flash, redirect, url_for, render_template
 from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField, validators
 import jaydebeapi, os, json
-import sqlalchemy.pool as pool
-import jpype
 
-mod = Blueprint('base', __name__)		
+from app import app
 
 class LoginForm(Form):
     username = TextField('Username', [validators.Required()])
     password = PasswordField('Password')	
 	
-
-@mod.route('/login', methods=['GET', 'POST'])
-def login():
-
-	form = LoginForm()
-	
-	if form.validate_on_submit():
-
-		session['username'] = form.username.data
-		session['password'] = form.password.data
-
-		if logged_in():
-			return redirect(url_for('userManagement.overview'))		
-		
-	return render_template("login.html", form=form)	
-	
-def connect():
-
-	path = os.path.join(os.getcwd(),'app','jt400.jar')
-	connection = jaydebeapi.connect('com.ibm.as400.access.AS400JDBCDriver', 'jdbc:as400://10.195.2.70;ccsid=285;translate binary=true;naming=system;prompt=false;libraries=CNEODTA002', [session['username'],  session['password']], path,)
-
-	return connection
-
-mypool =  pool.QueuePool(connect, max_overflow=10, pool_size=5)
-
-	
 def execute_query(sql, parms = []):
 	
-	cursor = mypool.connect()
-	if not jpype.isThreadAttachedToJVM():
-		jpype.attachThreadToJVM()
+	path = os.path.join(os.getcwd(),'app','jt400.jar')
+	connection = jaydebeapi.connect('com.ibm.as400.access.AS400JDBCDriver', 'jdbc:as400://10.195.2.70;ccsid=285;translate binary=true;naming=system;prompt=false;', [session['username'],  session['password']], path,)
 
 	cursor = connection.cursor()
 	cursor.execute(sql, parms)
 
 	return cursor
+
+def getCurrentLibrary(firmcode):
+
+	sql = '''SELECT envllb              
+					  FROM envconfig.envlib    
+					WHERE envlen = 'CNEOLIV' || {} 
+					  AND envlap = '*BASE'     
+					  AND envlsq = 10'''.format("'" + str(firmcode) + "'")   
+
+	result = execute_query(sql)
+	return result.fetchone()				  
 	
 def logged_in():
 
@@ -62,3 +44,102 @@ def logged_in():
 		return False
 	
 	return True	
+
+#########################################################
+# Classes
+#########################################################
+class User:
+
+	def __init__(self):
+		self.username = None
+		self.forename = None
+		self.surname = None
+
+
+
+#########################################################
+# Methods
+#########################################################
+def getCustomers():
+
+	sql = '''SELECT enveen FROM envconfig.envenv WHERE enveen LIKE 'CNEOLIV%' '''
+
+	results = execute_query(sql)
+	
+	return results.fetchall()
+
+
+def getUsers(firmcode):
+
+	library = getCurrentLibrary(firmcode)
+	library = library[0]
+
+	sql = 'SELECT userid, forename, surname FROM ' + library.strip() + '.user'
+
+	results = execute_query(sql)
+	
+	return results.fetchall()
+
+def getUser(firmcode, userid):
+
+	library = getCurrentLibrary(firmcode)
+	library = library[0]
+
+	sql = 'SELECT userid, forename, surname FROM ' + library.strip() + '.user'
+
+	result = execute_query(sql)	
+	result = result.fetchone()
+
+	user = User()
+	user.userid = result[0]
+	user.forename = result[1]
+	user.surname = result[2]
+	
+	return user
+
+
+#########################################################
+# Routes
+#########################################################
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+	form = LoginForm()
+	
+	if form.validate_on_submit():
+
+		session['username'] = form.username.data
+		session['password'] = form.password.data
+
+		if logged_in():
+			return redirect(url_for('customerList'))		
+		
+	return render_template("login.html", form=form)	
+
+
+@app.route('/customer/')
+def customerList():
+	 
+	if not logged_in():
+		return redirect(url_for('login'))	
+	 
+	return render_template("customer_list.html", customers=getCustomers())
+
+
+
+@app.route('/customer/<firmcode>/')
+def userList(firmcode):
+	 
+	if not logged_in():
+		return redirect(url_for('login'))	
+	 
+	return render_template("user_list.html", users=getUsers(firmcode), firmcode=firmcode)
+
+
+@app.route('/customer/<firmcode>/<userid>')
+def userDetail(firmcode, userid):
+	 
+	if not logged_in():
+		return redirect(url_for('login'))	
+	 
+	return render_template("user_detail.html", user=getUser(firmcode, userid), firmcode=firmcode)	
