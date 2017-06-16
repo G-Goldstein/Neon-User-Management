0a1cb1f2fdca411081cd8f0bdabccc64f233eecf
Alex Raison <alexraison@gmail.com>
2017-06-16 13:37:26 +0100

    Change string to boolean

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,14 @@ import os
 
 app = Flask(__name__)
 
-app.config['WTF_CSRF_ENABLED'] = os.environ['WTF_CSRF_ENABLED']
+if os.environ['WTF_CSRF_ENABLED'] == 'False':
+
+	app.config['WTF_CSRF_ENABLED'] = False
+
+else:
+
+	app.config['WTF_CSRF_ENABLED'] = True 
+	
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 from app import views
